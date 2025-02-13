8d28d585d2adc7c626f5ae5a869cbb5ee9d72b09
Adnan Bajric <adnanbajric74@gmail.com>
2025-02-13 12:04:15 +0100

    Header for Register fix

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -33,9 +33,6 @@
                 <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>login/index">Login</a>
                 </li>
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "register/index")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>register/index">Register</a>
-                </li>
             <?php } ?>
         </ul>
 
@@ -71,6 +68,10 @@
                     echo ' class="active" ';
                 } ?> >
                     <a href="<?php echo Config::get('URL'); ?>admin/">Admin</a>
+                </li>
+
+                <li <?php if (View::checkForActiveControllerAndAction($filename, "register/index")) { echo ' class="active" '; } ?> >
+                    <a href="<?php echo Config::get('URL'); ?>register/index">Register</a>
                 </li>
 
             <?php if (Session::userIsLoggedIn()) { ?>
