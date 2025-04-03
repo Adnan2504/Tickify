df65b749196892d0590767b577efb060fc7c3c28
Phonk Lover <phonklover@example.com>
2025-04-03 15:56:31 +0200

    index update

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -23,8 +23,8 @@
         <!-- Render system feedback messages -->
         <?php $this->renderFeedbackMessages(); ?>
 
-        <h2 class="text-2xl font-bold text-blue-600 mb-3 inline-flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
+        <h2 class="text-2xl font-bold text-blue-600 mb-3 inline-flex items-center justify-center w-full text-center">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
                  stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                       d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"/>
