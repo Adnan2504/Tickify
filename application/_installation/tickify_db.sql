64d4a6b341b8a2dd0c7df52395800fc00f3a43ef
Adnan Bajric <adnanbajric74@gmail.com>
2025-04-15 08:52:34 +0200

    TIckify DB

--- application/_installation/tickify_db.sql
+++ application/_installation/tickify_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 04, 2025 at 10:34 AM
+-- Generation Time: Apr 15, 2025 at 08:51 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,18 +42,27 @@ CREATE TABLE `messages` (
 --
 
 INSERT INTO `messages` (`message_id`, `ticket_id`, `sender_id`, `message_text`, `attachment_path`, `sent_at`, `is_internal`) VALUES
-(7, 1, 1, 'dwadwadwa', '67aa5d66a255d_IMG_6744.HEIC', '2025-02-10 20:11:19', 0),
 (8, 3, 1, 'Test3', NULL, '2025-02-10 20:12:53', 0),
-(9, 1, 1, 'test', 'images/1/67aa5fa972d17_istockphoto-1160791767-612x612.jpg', '2025-02-10 20:20:57', 0),
-(10, 1, 1, 'test', NULL, '2025-02-10 20:38:38', 0),
 (11, 4, 1, 'Test Image', 'images/1/67bef2316a112_board-361516_1280.webp', '2025-02-26 10:51:29', 0),
 (12, 4, 1, 'Test Image', 'images/1/67bef2574a9df_board-361516_1280.webp', '2025-02-26 10:52:07', 0),
 (13, 4, 1, 'more testing', 'images/1/67c02da528d9c_board-361516_1280.webp', '2025-02-27 09:17:25', 0),
 (14, 4, 1, 'Test', NULL, '2025-02-27 09:17:31', 0),
 (15, 4, 1, 'Test by Admin', NULL, '2025-02-27 09:17:37', 0),
-(16, 7, 1, 'dwadwadwa', 'images/1/67c5a196566a6_board-361516_1280.webp', '2025-03-03 12:33:26', 0),
 (17, 3, 1, 'test', 'images/1/67c5c561168fe_board-361516_1280.webp', '2025-03-03 15:06:09', 0),
-(18, 5, 1, 'Hello', 'images/1/67c6ac678ae85_board-361516_1280.webp', '2025-03-04 07:31:51', 0);
+(18, 5, 1, 'Hello', 'images/1/67c6ac678ae85_board-361516_1280.webp', '2025-03-04 07:31:51', 0),
+(20, 5, 1, 'test', NULL, '2025-03-18 11:05:30', 0),
+(21, 5, 1, 'test', NULL, '2025-03-18 11:08:20', 0),
+(22, 5, 1, 'more tesing', NULL, '2025-03-18 11:08:24', 0),
+(23, 5, 1, 'a', NULL, '2025-03-18 11:10:01', 0),
+(24, 5, 1, 'a', NULL, '2025-03-18 11:10:03', 0),
+(25, 5, 1, 'a', NULL, '2025-03-18 11:10:05', 0),
+(26, 5, 1, 'a', NULL, '2025-03-18 11:10:06', 0),
+(33, 5, 1, '213', NULL, '2025-03-27 06:49:18', 0),
+(34, 5, 1, 'Test', NULL, '2025-03-27 06:49:21', 0),
+(35, 5, 1, 'horse', 'images/1/67e4f4fc30490_123.png.jpg', '2025-03-27 06:49:32', 0),
+(66, 27, 14, 'My screen is black', NULL, '2025-04-04 06:07:08', 0),
+(67, 27, 14, 'My screen is black this is image of it', 'images/14/67ef771f68c3a_download.jpg', '2025-04-04 06:07:27', 0),
+(68, 27, 14, '321', 'images/14/67ef775423fcf_download.jpg', '2025-04-04 06:08:20', 0);
 
 -- --------------------------------------------------------
 
@@ -78,14 +87,16 @@ CREATE TABLE `support_tickets` (
 --
 
 INSERT INTO `support_tickets` (`id`, `subject`, `description`, `priority`, `attachment_path`, `category`, `created_by`, `created_at`, `status`) VALUES
-(1, 'Test Ticket Subject', 'This is a test description for the support ticket.', 'mid', '/path/to/test-image.png', 'General Inquiry', 1, '2025-02-06 08:47:20', 'resolved'),
-(3, 'test123', 'test123', 'high', NULL, 'test123', 1, '2025-02-06 09:22:11', 'resolved'),
-(4, 'Test ticket by adnan', 'some random text', 'low', NULL, 'test', 1, '2025-02-11 10:12:10', 'open'),
+(3, 'test123', 'test123', 'high', NULL, 'test', 1, '2025-02-06 09:22:11', 'resolved'),
+(4, 'Test ticket by adnan', 'some random text', 'low', NULL, 'test123', 1, '2025-02-11 10:12:10', 'resolved'),
 (5, 'test', 'test', 'low', NULL, 'test', 2, '2025-02-13 10:38:19', 'open'),
-(6, 'test_file', 'file', 'high', NULL, 'file', 2, '2025-02-13 10:41:19', 'open'),
-(7, 'Test by admin', 'Admin testing :D', 'high', NULL, 'Random Category', 1, '2025-02-27 09:10:22', 'open'),
-(8, 'Subject of ticket', 'random ticket', 'mid', NULL, 'test', 1, '2025-02-28 07:51:31', 'open'),
-(9, 'Test', 'test', 'high', NULL, 'test iwas', 1, '2025-03-03 12:32:16', 'open');
+(6, 'test_file', 'file', 'high', NULL, 'test', 2, '2025-02-13 10:41:19', 'open'),
+(8, 'Subject of ticket', 'random ticket', 'low', NULL, 'Support', 1, '2025-02-28 07:51:31', 'open'),
+(14, 'Ordering test', 'Ordering test', 'low', NULL, 'Improvement', 1, '2025-03-27 07:01:02', 'open'),
+(15, 'oder 15', 'oder 15', 'low', NULL, 'Bug', 1, '2025-03-27 07:01:22', 'open'),
+(16, 'Adnan', 'ad', 'low', NULL, 'Bug', 1, '2025-03-27 07:59:52', 'open'),
+(26, '1', '1', 'low', NULL, 'Bug', 14, '2025-04-04 04:58:13', 'open'),
+(27, 'My screen is black i need it until tomorrow', 'My screen is black', 'low', NULL, 'Support', 14, '2025-04-04 06:06:56', 'open');
 
 -- --------------------------------------------------------
 
@@ -120,12 +131,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `session_id`, `user_name`, `user_password_hash`, `user_email`, `user_active`, `user_deleted`, `user_account_type`, `user_has_avatar`, `user_remember_me_token`, `user_creation_timestamp`, `user_suspension_timestamp`, `user_last_login_timestamp`, `user_failed_logins`, `user_last_failed_login`, `user_activation_hash`, `user_password_reset_hash`, `user_password_reset_timestamp`, `user_provider_type`) VALUES
-(1, '481ru06i034glo3ofaple9t808', 'demo', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo@demo.com', 1, 0, 7, 1, NULL, 1422205178, NULL, 1741073433, 0, NULL, NULL, NULL, NULL, 'DEFAULT'),
-(2, NULL, 'demo2', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo2@demo.com', 1, 0, 1, 0, NULL, 1422205178, NULL, 1739443354, 0, NULL, NULL, NULL, NULL, 'DEFAULT'),
-(3, NULL, 'adnan', '$2y$10$xBjWm9OnIwh1pCoAtv2iYePNgtTBHAC2sbaCFH6UkoB8jyb6bK0/y', 'adnan2@gmail.com', 0, 0, 1, 0, NULL, 1739444047, NULL, NULL, 0, NULL, 'f079e2bd26575d5fdaa2d318040c62603488d5b2a956165813b73500196b69d728a3cf78eb60cc58', NULL, NULL, 'DEFAULT'),
-(4, NULL, 'random', '$2y$10$UtW4jYeFrPpeKFFZWQs6rObgRcM7PmRoxlZF/riTYJM2dmTvw9QwO', 'random@gmail.com', 1, 0, 1, 0, NULL, 1739444430, NULL, NULL, 0, NULL, '2a2ee57fb6b760e49665ba4e42bc2d062884b6eb366946892f82bf10c41409c49d6c6ce97e0fb2bb', NULL, NULL, 'DEFAULT'),
-(5, NULL, 'random2', '$2y$10$iSbj6SsG6DJdds11i69y9.HcNt1yYmDROB.MHPasweLYOAWDlHAc6', 'random2@gmail.com', 1, 0, 1, 0, NULL, 1739444489, NULL, NULL, 0, NULL, '7996bde20ec7fb21e6d04546db6c734879f643ff56c9c7d8feaf1d8d80d97c664aa26d1680c3bc00', NULL, NULL, 'DEFAULT'),
-(6, NULL, 'finishtest', '$2y$10$2inOlK8F9bkTA5H7.Hw2GeYCvbb2Z9xV5BKc5Js8I/dm82qaKgcWy', 'random321@gmail.com', 1, 0, 1, 0, NULL, 1739444553, NULL, NULL, 0, NULL, '407a6aca61a47003291048ed25914fd17ace0dba9e518d578ec27483f9a32aad52fbb49637c4560a', NULL, NULL, 'DEFAULT');
+(1, 'i4l0pagj6sbegjkuusibio5a1i', 'Admin', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'adnanbajric74@gmail.com', 1, 0, 7, 1, NULL, 1422205178, NULL, 1743747711, 0, NULL, NULL, '7a36dbf81316e553f77a34798a4dfdd316c307ead9304df2b512cd7b0ff47a4b3ab2a598c749e11d', 1741330374, 'DEFAULT'),
+(2, NULL, 'demo2', '$2y$10$OvprunjvKOOhM1h9bzMPs.vuwGIsOqZbw88rzSyGCTJTcE61g5WXi', 'demo2@demo.at', 1, 0, 5, 0, NULL, 1422205178, NULL, 1739443354, 0, NULL, NULL, NULL, NULL, 'DEFAULT'),
+(14, NULL, 'adnan_2504', '$2y$10$PLUmoAlkY/vDqzwC6WomWO69jzh/sNkR.7Xn2EBG2XxJpXOneHNde', 'adnanbajric@gmail.com', 1, 0, 7, 1, NULL, 1742542505, NULL, 1743746745, 0, NULL, 'f1e909d280ec248c2ac71b245881718ac37bdf844f1c06290ecbcc373c904dd99aa14dffa51074be', NULL, NULL, 'DEFAULT'),
+(19, NULL, 'livedemo', '$2y$10$YSjH7.kutw.GgA330hB.3ecSHe7hoqdwbiCp7p2GgsAl4mfiJMRQC', 'livedemo@gmail.com', 1, 0, 1, 0, NULL, 1743746730, NULL, NULL, 3, 1743746743, 'a0bd72d0cd176e4d7a15f5014f5c822585649605681fa6ee88c8485c7eab9c4708ece1bbf3e37f79', NULL, NULL, 'DEFAULT'),
+(20, NULL, 'adnan123', '$2y$10$Eg8fb64q.Iq9H3rPn2TmqesLT.3b2y9pgC6PCDoFvfoL0aI4t4qba', 'adnan123321321@gmail.com', 1, 0, 1, 0, NULL, 1743747602, NULL, 1743747613, 0, NULL, 'cea2b749010b2090837a01542f60404b6bf78656eaa2227c40235114576149e62fd66a0a051f4c3d', NULL, NULL, 'DEFAULT');
 
 -- --------------------------------------------------------
 
@@ -144,8 +154,7 @@ CREATE TABLE `user_groups_long` (
 --
 
 INSERT INTO `user_groups_long` (`id`, `account_type`, `lang`) VALUES
-(1, 1, 'Guest'),
-(2, 2, 'User'),
+(1, 1, 'User'),
 (3, 7, 'Admin'),
 (4, 5, 'Moderator');
 
@@ -158,8 +167,8 @@ INSERT INTO `user_groups_long` (`id`, `account_type`, `lang`) VALUES
 --
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`message_id`),
-  ADD KEY `ticket_id` (`ticket_id`),
-  ADD KEY `sender_id` (`sender_id`);
+  ADD KEY `messages_ibfk_1` (`ticket_id`),
+  ADD KEY `messages_ibfk_2` (`sender_id`);
 
 --
 -- Indexes for table `support_tickets`
@@ -189,19 +198,19 @@ ALTER TABLE `user_groups_long`
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT for table `support_tickets`
 --
 ALTER TABLE `support_tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index', AUTO_INCREMENT=7;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index', AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `user_groups_long`
@@ -217,8 +226,8 @@ ALTER TABLE `user_groups_long`
 -- Constraints for table `messages`
 --
 ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `support_tickets` (`id`),
-  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`);
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `support_tickets` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
